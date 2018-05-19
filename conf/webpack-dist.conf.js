f7e704dee274c4892b5db72659ba1dd45539e1fe
BraveVN <dungpa93@gmail.com>
2018-05-19 07:40:20 +0700

    update packagejson & webpack config

--- conf/webpack-dist.conf.js
+++ conf/webpack-dist.conf.js
@@ -49,10 +49,6 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': '"production"'
     }),
-    new webpack.optimize.UglifyJsPlugin({
-      output: {comments: false},
-      compress: {unused: true, dead_code: true, warnings: false} // eslint-disable-line camelcase
-    }),
     new ExtractTextPlugin('index-[contenthash].css'),
     new webpack.optimize.CommonsChunkPlugin({name: 'vendor'}),
     new webpack.LoaderOptionsPlugin({
@@ -68,5 +64,9 @@ module.exports = {
   entry: {
     app: `./${conf.path.src('index')}`,
     vendor: Object.keys(pkg.dependencies)
-  }
+  },
+  node: {
+    fs: "empty"
+  },
+  target: "node"
 };
